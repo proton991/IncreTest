5b9d503676afc4143d15773e97f03645ed05f4d1
proton991 <99615819@qq.com>
2023-05-09 15:17:54 +0800

    add package

---
+++ src/main/java/repository/PersonRepo.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.Person;
+
+public interface PersonRepo {
+
+    void savePerson(Person person);
+}
--- src/main/java/service/PersonServiceImpl.java
+++ src/main/java/service/PersonServiceImpl.java
@@ -1,10 +1,12 @@
 package service;
 
 import model.Person;
+import repository.PersonRepo;
 
 public class PersonServiceImpl implements PersonService {
+    PersonRepo repo;
     @Override
     public void addPerson(Person person) {
-
+        repo.savePerson(person);
     }
 }
